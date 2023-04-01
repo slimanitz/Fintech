1c59a648a71c24605b4bf6935c93b8369d9ae6bd
slimanePLF <slimaneber2001@gmail.com>
2023-04-01 23:08:36 +0200

    Auth middeleware up and running

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,31 +3,36 @@
 const jwt = require('jsonwebtoken');
 const User = require('../api/models/user');
 const { jwtSecret } = require('../config/vars');
+const APIError = require('../utils/api-error');
 
 // I made a specific login in this middleware where I've make a verification
 // if the user is using a good Agency ID and Iam checking as well on the database if it's coherent
 
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+const authenticateJWT = (role) => (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    // eslint-disable-next-line consistent-return
-    jwt.verify(token, jwtSecret, async (err, payload) => {
-      if (err || !payload) {
-        return res.sendStatus(403);
-      }
-      const { user } = payload;
-      const dbUser = await User.findById(user._id);
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+      // eslint-disable-next-line consistent-return
+      jwt.verify(token, jwtSecret, async (err, payload) => {
+        if (err || !payload) {
+          return res.sendStatus(403);
+        }
+        const { user } = payload;
+        const dbUser = await User.findById(user._id);
 
-      if (dbUser && dbUser.agency.equals(user.agency)) {
-        req.user = dbUser;
-        return next();
-      }
+        if (dbUser && dbUser.role === role) {
+          req.user = dbUser;
+          return next();
+        }
+        res.sendStatus(401);
+      });
+    } else {
       res.sendStatus(401);
-    });
-  } else {
-    res.sendStatus(401);
+    }
+  } catch (e) {
+    throw new APIError({ message: 'Error caught on the Auth midleware' });
   }
 };
 
