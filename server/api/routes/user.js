496a4939465698349f6bff890a58ceced3d929f4
slimanePLF <slimaneber2001@gmail.com>
2023-04-03 00:46:21 +0200

    Removing routes protection for dev purpose and cleaning routing

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -3,37 +3,38 @@ const {
   create, get,
   getAll, update, remove, login, createUserAccount,
   getAllUserAccounts, getUserAccount,
-  updateUserAccount, createUserAccountCreditCard,
-  getAllUserAccountCreditCards,
-  getUserAccountCreditCard, updateUserAccountCreditCard,
+  updateUserAccount, createUserCreditCard,
+  getAllUserCreditCards,
+  getUserCreditCard, updateUserCreditCard,
   createUserTransaction,
   getAllUserTransactions,
   getUserTransaction,
 } = require('../controllers/user');
 const { userRolesEnum } = require('../../utils/enums');
 const authenticateJWT = require('../../middlewares/auth');
+const checkUserAsset = require('../../middlewares/security');
 
 const router = express.Router();
 
 // USERS
-router.post('/', authenticateJWT([userRolesEnum.ADMIN, userRolesEnum.CLIENT]), create);
-router.get('/:id', authenticateJWT([userRolesEnum.ADMIN]), get);
+router.post('/', create);
+router.get('/:id', get);
 router.post('/login', login);
-router.get('/', authenticateJWT([userRolesEnum.ADMIN]), getAll);
-router.patch('/:id', authenticateJWT([userRolesEnum.ADMIN]), update);
-router.delete('/:id', authenticateJWT([userRolesEnum.ADMIN]), remove);
+router.get('/', getAll);
+router.patch('/:id', update);
+router.delete('/:id', remove);
 
 // Accounts
 router.post('/:userId/accounts', authenticateJWT([userRolesEnum.CLIENT]), createUserAccount);
-router.get('/:userId/accounts', authenticateJWT([userRolesEnum.CLIENT]), getAllUserAccounts);
-router.get('/:userId/accounts/:accountId', authenticateJWT([userRolesEnum.CLIENT]), getUserAccount);
-router.patch('/:userId/accounts/:accountId', authenticateJWT([userRolesEnum.CLIENT]), updateUserAccount);
+router.get('/:userId/accounts', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, getAllUserAccounts);
+router.get('/:userId/accounts/:accountId', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, getUserAccount);
+router.patch('/:userId/accounts/:accountId', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, updateUserAccount);
 
 // Credit cards
-router.post('/:userId/accounts/:accountId/credit-cards', authenticateJWT([userRolesEnum.CLIENT]), createUserAccountCreditCard);
-router.get('/:userId/accounts/:accountId/credit-cards', authenticateJWT([userRolesEnum.CLIENT]), getAllUserAccountCreditCards);
-router.get('/:userId/accounts/:accountId/credit-cards/:creditCardId', authenticateJWT([userRolesEnum.CLIENT]), getUserAccountCreditCard);
-router.patch('/:userId/accounts/:accountId/credit-cards/:creditCardId', authenticateJWT([userRolesEnum.CLIENT]), updateUserAccountCreditCard);
+router.post('/:userId/accounts/:accountId/credit-cards', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, createUserCreditCard);
+router.get('/:userId/credit-cards', authenticateJWT([userRolesEnum.CLIENT]), getAllUserCreditCards);
+router.get('/:userId/credit-cards/:creditCardId', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, getUserCreditCard);
+router.patch('/:userId/credit-cards/:creditCardId', authenticateJWT([userRolesEnum.CLIENT]), checkUserAsset, updateUserCreditCard);
 
 // Transactions
 router.post('/:userId/accounts/:accountId/transactions', authenticateJWT([userRolesEnum.CLIENT]), createUserTransaction);
