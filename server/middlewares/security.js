8909487cbddeeec843e99fb347097e0ee3d82273
slimanePLF <slimaneber2001@gmail.com>
2023-04-03 00:47:29 +0200

    Implementing security middleware

---
+++ server/middlewares/security.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-underscore-dangle */
+const httpStatus = require('http-status');
+const APIError = require('../utils/api-error');
+const Account = require('../api/models/account');
+const CreditCard = require('../api/models/creditCard');
+const Transaction = require('../api/models/transaction');
+
+// I made a specific login in this middleware where I've make a verification
+// if the user is using a good Agency ID and Iam checking as well on the database if it's coherent
+
+const checkUserAsset = async (req, res, next) => {
+  const {
+    userId, accountId, creditCardId, transactionId,
+  } = req.params;
+
+  const userAccounts = (creditCardId) && await Account.find({ userId });
+  const creditCards = (creditCardId) && await CreditCard.find({ userId });
+  const transactions = (transactionId) && await Transaction.find({ userId });
+
+  if (!userAccounts.find((e) => e._id.equals(accountId))) throw new APIError({ message: 'Trying to access to someone else assets', status: httpStatus.CONFLICT });
+  if (!creditCards.find((e) => e._id.equals(creditCardId))) throw new APIError({ message: 'Trying to access to someone else assets', status: httpStatus.CONFLICT });
+  if (!transactions.find((e) => e._id.equals(transactionId))) throw new APIError({ message: 'Trying to access to someone else assets', status: httpStatus.CONFLICT });
+
+  return next();
+};
+
+module.exports = checkUserAsset;
