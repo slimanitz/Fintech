6fac0a5d566f03526feca7198777284b65334cdf
slimanePLF <slimaneber2001@gmail.com>
2023-04-02 22:21:51 +0200

    Quick fix with implementation of currencies

--- server/api/models/account.js
+++ server/api/models/account.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 // eslint-disable-next-line no-unused-vars
-const { faker } = require('@faker-js/faker');
 const mongoClient = require('../../config/database');
 const { accountTypesEnum } = require('../../utils/enums');
 
 const accountSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Types.ObjectId },
-    iban: { type: String, default: faker.finance.iban() },
+    userId: { type: mongoose.Types.ObjectId, required: true },
+    iban: { type: String, unique: true },
     type: { type: String, enum: Object.values(accountTypesEnum), default: accountTypesEnum.BASIC },
     balance: { type: Number, default: 0 },
     isActive: { type: Boolean, default: true },
+    currency: { type: String, required: true },
 
   },
   { timestamps: true },
