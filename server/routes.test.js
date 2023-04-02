d00a25948862ebad3bad3c24b54ee84ab80fdbd7
slimanePLF <slimaneber2001@gmail.com>
2023-04-03 01:34:13 +0200

    user test done WIP: Account test

--- server/routes.test.js
+++ server/routes.test.js
@@ -1,209 +1,191 @@
-// /* eslint-disable no-underscore-dangle */
-// const request = require('supertest');
-// const { default: mongoose } = require('mongoose');
-// const app = require('./config/server');
-// const connect = require('./config/database');
-// const { mongoUrl, host, jwtSecret } = require('./config/vars');
+/* eslint-disable no-underscore-dangle */
+const request = require('supertest');
+const { default: mongoose } = require('mongoose');
+const httpStatus = require('http-status');
+const app = require('./config/server');
+const connect = require('./config/database');
+const { mongoUrl, host, jwtSecret } = require('./config/vars');
 
-// describe('Check before launching tests', () => {
-//   beforeAll(async () => {
-//     await connect();
-//   });
+let token = 'Bearer ';
 
-//   test('Should make sure that we are using the test env variables ', () => {
-//     console.log('====================================');
-//     console.log(mongoUrl);
-//     console.log(host);
-//     console.log(jwtSecret);
-//     mongoose.connection.db.dropDatabase(() => console.log('Database dropped succesfully'));
-//     console.log('====================================');
-//     expect(process.env.APP_ENV).toEqual('test');
-//   });
-// });
+describe('Check before launching tests', () => {
+  beforeAll(async () => {
+    await connect();
+  });
 
-// describe('Testing API Endpoints API', () => {
-//   let server;
+  test('Should make sure that we are using the test env variables ', async () => {
+    console.log('====================================');
+    console.log(mongoUrl);
+    console.log(host);
+    console.log(jwtSecret);
+    await mongoose.connection.db.dropDatabase(() => console.log('Database dropped succesfully'));
+    console.log('====================================');
+    expect(process.env.APP_ENV).toEqual('test');
+  });
+});
 
-//   beforeAll(() => {
-//     server = app.listen(8080, async () => {
-//       await connect();
-//       console.log('Express server started on port 8080');
-//     });
-//   });
+describe('Testing Client API Endpoints', () => {
+  let server;
 
-//   afterAll(() => {
-//     server.close();
-//   });
-//   let agency = {
-//     name: 'orpi',
-//     address: 'une address',
-//     city: 'Paris',
-//     postalCode: '75003',
-//     website: 'https://www.orpi.com',
-//   };
+  beforeAll(() => {
+    server = app.listen(8080, async () => {
+      await connect();
+      console.log('Express server started on port 8080');
+    });
+  });
 
-//   let user = {
-//     email: 'slimane234@gmail.com',
-//     password: 'password',
-//     name: 'Slimaneber',
-//   };
+  afterAll(() => {
+    server.close();
+  });
 
-//   afterEach(() => {
-//     agency = {
-//       _id: agency._id,
-//       name: 'orpi',
-//       address: 'une address',
-//       city: 'Paris',
-//       postalCode: '75003',
-//       website: 'https://www.orpi.com',
-//     };
-//     user = {
-//       _id: user._id,
-//       email: 'slimane.berrada.01@gmail.com',
-//       password: 'password',
-//       name: 'Slimaneber',
-//       agency: agency._id,
-//     };
-//   });
+  const user = {
+    email: 'slimaneber@gmail.com',
+    password: 'password',
+    name: 'Slimaneber',
+  };
+  const account = {
+    name: 'orpi',
+    address: 'une address',
+    city: 'Paris',
+    postalCode: '75003',
+    website: 'https://www.orpi.com',
+  };
 
-//   describe('POST /api/agencies', () => {
-//     it('should create a agency', async () => {
-//       const res = await request(app).post('/api/agencies').send(agency);
-//       agency._id = res.body._id;
-//       expect(res.status).toEqual(200);
-//       expect(res.body.name).toEqual(agency.name);
-//     });
-//   });
+  // ================USER=================
 
-//   describe('GET /api/agencies', () => {
-//     it('should return all agencies', async () => {
-//       const res = await request(app).get('/api/agencies');
-//       expect(res.status).toEqual(200);
-//       expect(res.body).toContainEqual(agency);
-//     });
-//   });
+  describe('POST /api/users', () => {
+    test('should create an account', async () => {
+      console.log(user);
+      const res = await request(app).post('/api/users').send(user);
+      user._id = res.body._id;
+      expect(res.status).toEqual(200);
+      expect(res.body.email).toEqual(user.email);
+    });
+  });
 
-//   describe('GET /api/agencies/:id', () => {
-//     it('should return a single agency by id', async () => {
-//       const res = await request(app).get(`/api/agencies/${agency._id}`);
-//       expect(res.status).toEqual(200);
-//       expect(res.body.name).toEqual(agency.name);
-//     });
+  describe('POST /api/users/login', () => {
+    test('Should login an return a token', async () => {
+      const { email, password } = user;
+      const res = await request(app).post('/api/users/login').send({ email, password });
+      expect(res.status).toEqual(200);
+      token += res.body.token;
+    });
+  });
 
-//     it('should return 404 if agency is not found', async () => {
-//       const res = await request(app).get('/api/agencies/99');
-//       expect(res.status).toEqual(404);
-//     });
-//   });
+  describe('GET /api/users/:id', () => {
+    test('should return a single user by id', async () => {
+      const res = await request(app).get(`/api/users/${user._id}`);
+      expect(res.status).toEqual(200);
+      expect(res.body.email).toEqual(user.email);
+    });
 
-//   describe('PATCH /api/agencies/:id', () => {
-//     it('should update an existing agency', async () => {
-//       const updatedAgency = {
-//         name: 'La foret',
-//         address: 'une address',
-//         city: 'Paris',
-//         postalCode: '75003',
-//         website: 'https://www.orpi.com',
-//       };
-//       const res = await request(app)
-//         .patch(`/api/agencies/${agency._id}`)
-//         .send(updatedAgency);
-//       expect(res.status).toEqual(200);
-//       expect(res.body).toEqual({ ...updatedAgency, _id: agency._id });
-//     });
+    test('should return 404 if agency is not found', async () => {
+      const res = await request(app).get('/api/users/99');
+      expect(res.status).toEqual(404);
+    });
+  });
 
-//     it('should return 404 if agency is not found', async () => {
-//       const res = await request(app).put('/api/agencies/99').send({ name: 'La foret' });
-//       expect(res.status).toEqual(404);
-//     });
-//   });
+  describe('PATCH /api/users/:id', () => {
+    it('should update an existing user', async () => {
+      const updatedUser = {
+        name: 'Sliman',
+        email: 'slimaneber2001@gmail.com',
+      };
+      const res = await request(app)
+        .patch(`/api/users/${user._id}`)
+        .send(updatedUser);
+      console.log(user._idc);
+      expect(res.status).toEqual(200);
+      expect(res.body.name).toEqual(updatedUser.name);
+    });
 
-//   describe('DELETE /api/agencies/:id', () => {
-//     it('should delete an existing agency', async () => {
-//       const res = await request(app).delete(`/api/agencies/${agency._id}`);
-//       expect(res.status).toEqual(200);
-//     });
+    it('should return 404 if user is not found', async () => {
+      const res = await request(app).put('/api/users/99').send({ name: 'test' });
+      expect(res.status).toEqual(404);
+    });
+  });
 
-//     it('should return 404 if agency is not found', async () => {
-//       const res = await request(app).delete('/api/agencies/99');
-//       expect(res.status).toEqual(404);
-//     });
-//   });
+  // ================Account=================
 
-//   // ================USER=================
+  describe('POST /api/users/:userId/accounts', () => {
+    it('should create an account ', async () => {
+      const res = await (await request(app).post(`/api/users/${user._id}/accounts`)).set('Authorization', token).send(account);
+      account._id = res.body._id;
+      expect(res.status).toEqual(200);
+      expect(res.body.balance).toEqual(0);
+    });
 
-//   describe('POST /api/users', () => {
-//     it('should create a user', async () => {
-//       const res = await request(app).post('/api/users').send(user);
-//       user._id = res.body._id;
-//       expect(res.status).toEqual(200);
-//       expect(res.body.name).toEqual(user.name);
-//     });
-//   });
+    it('Authentication check on /api/users/:userId/accounts', async () => {
+      it('should return FORBIDDEN', async () => {
+        const res = await request(app).post(`/api/users/${user._id}/accounts`).send(account);
+        expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+      });
+    });
+  });
 
-//   describe('GET /api/users', () => {
-//     it('should return all users', async () => {
-//       const res = await request(app).get('/api/users');
-//       expect(res.status).toEqual(200);
-//       expect(res.body[0].email).toEqual(user.email);
-//     });
-//   });
+  // describe('GET /api/users', () => {
+  //   it('should return all users', async () => {
+  //     const res = await request(app).get('/api/users');
+  //     expect(res.status).toEqual(200);
+  //     expect(res.body[0].email).toEqual(user.email);
+  //   });
+  // });
 
-//   describe('GET /api/users/:id', () => {
-//     it('should return a single user by id', async () => {
-//       const res = await request(app).get(`/api/users/${user._id}`);
-//       expect(res.status).toEqual(200);
-//       expect(res.body.name).toEqual(user.name);
-//     });
+  // describe('GET /api/users/:id', () => {
+  //   it('should return a single user by id', async () => {
+  //     const res = await request(app).get(`/api/users/${user._id}`);
+  //     expect(res.status).toEqual(200);
+  //     expect(res.body.name).toEqual(user.name);
+  //   });
 
-//     it('should return 404 if user is not found', async () => {
-//       const res = await request(app).get('/api/users/99');
-//       expect(res.status).toEqual(404);
-//     });
-//   });
+  //   it('should return 404 if user is not found', async () => {
+  //     const res = await request(app).get('/api/users/99');
+  //     expect(res.status).toEqual(404);
+  //   });
+  // });
 
-//   describe('POST /api/users/login', () => {
-//     it('should return a jwt', async () => {
-//       const res = await request(app).post('/api/users/login').send({ email: user.email, password: user.password });
-//       expect(res.status).toEqual(200);
-//     });
+  // describe('POST /api/users/login', () => {
+  //   it('should return a jwt', async () => {
+  //     const res = await request(app).post('/api/users/login').send({ email: user.email, password: user.password });
+  //     expect(res.status).toEqual(200);
+  //   });
 
-//     it('Should return code 401 on wrong credentials', async () => {
-//       const res = await request(app).post('/api/users/login').send({ email: user.email, password: 'notpassword' });
-//       expect(res.status).toEqual(401);
-//     });
-//   });
+  //   it('Should return code 401 on wrong credentials', async () => {
+  //     const res = await request(app).post('/api/users/login').send({ email: user.email, password: 'notpassword' });
+  //     expect(res.status).toEqual(401);
+  //   });
+  // });
 
-//   describe('PATCH /api/users/:id', () => {
-//     it('should update an existing user', async () => {
-//       const updatedUser = {
-//         email: 'slimane.berrada.01@gmail.com',
-//         password: 'password',
-//         name: 'Slimane',
-//         agency: agency._id,
-//       };
-//       const res = await request(app)
-//         .patch(`/api/users/${user._id}`)
-//         .send(updatedUser);
-//       expect(res.status).toEqual(200);
-//       expect(res.body.name).toEqual(updatedUser.name);
-//     });
+  // describe('PATCH /api/users/:id', () => {
+  //   it('should update an existing user', async () => {
+  //     const updatedUser = {
+  //       email: 'slimane.berrada.01@gmail.com',
+  //       password: 'password',
+  //       name: 'Slimane',
+  //       agency: agency._id,
+  //     };
+  //     const res = await request(app)
+  //       .patch(`/api/users/${user._id}`)
+  //       .send(updatedUser);
+  //     expect(res.status).toEqual(200);
+  //     expect(res.body.name).toEqual(updatedUser.name);
+  //   });
 
-//     it('should return 404 if user is not found', async () => {
-//       const res = await request(app).put('/api/users/99').send({ name: 'Slimane' });
-//       expect(res.status).toEqual(404);
-//     });
-//   });
+  //   it('should return 404 if user is not found', async () => {
+  //     const res = await request(app).put('/api/users/99').send({ name: 'Slimane' });
+  //     expect(res.status).toEqual(404);
+  //   });
+  // });
 
-//   describe('DELETE /api/users/:id', () => {
-//     it('should delete an existing user', async () => {
-//       const res = await request(app).delete(`/api/users/${user._id}`);
-//       expect(res.status).toEqual(200);
-//     });
+  // describe('DELETE /api/users/:id', () => {
+  //   it('should delete an existing user', async () => {
+  //     const res = await request(app).delete(`/api/users/${user._id}`);
+  //     expect(res.status).toEqual(200);
+  //   });
 
-//     it('should return 404 if user is not found', async () => {
-//       const res = await request(app).delete('/api/users/99');
-//       expect(res.status).toEqual(404);
-//     });
-//   });
-// });
+  //   it('should return 404 if user is not found', async () => {
+  //     const res = await request(app).delete('/api/users/99');
+  //     expect(res.status).toEqual(404);
+  //   });
+  // });
+});
