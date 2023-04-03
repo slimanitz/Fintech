1054ab00ece77cbbf30eca1070bd70221ddfc966
slimanePLF <slimaneber2001@gmail.com>
2023-04-03 03:12:18 +0200

    WIP: tests

--- server/routes.test.js
+++ server/routes.test.js
@@ -51,6 +51,8 @@ describe('Testing Client API Endpoints', () => {
     website: 'https://www.orpi.com',
   };
 
+  let creditCard = {};
+
   // ================USER=================
 
   describe('POST /api/users', () => {
@@ -108,84 +110,127 @@ describe('Testing Client API Endpoints', () => {
   // ================Account=================
 
   describe('POST /api/users/:userId/accounts', () => {
-    it('should create an account ', async () => {
-      const res = await (await request(app).post(`/api/users/${user._id}/accounts`)).set('Authorization', token).send(account);
+    test('should create an account ', async () => {
+      const res = await request(app).post(`/api/users/${user._id}/accounts`).set('Authorization', token).send({ userId: user._id });
       account._id = res.body._id;
       expect(res.status).toEqual(200);
       expect(res.body.balance).toEqual(0);
     });
 
-    it('Authentication check on /api/users/:userId/accounts', async () => {
-      it('should return FORBIDDEN', async () => {
+    describe('Authentication check on POST /api/users/:userId/accounts', () => {
+      test('should return FORBIDDEN', async () => {
         const res = await request(app).post(`/api/users/${user._id}/accounts`).send(account);
         expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
       });
     });
   });
 
-  // describe('GET /api/users', () => {
-  //   it('should return all users', async () => {
-  //     const res = await request(app).get('/api/users');
+  describe('GET /api/users/:userId/accounts', () => {
+    test('should return all user accounts', async () => {
+      const res = await request(app).get(`/api/users/${user._id}/accounts`).set('Authorization', token);
+      expect(res.status).toEqual(200);
+      expect(res.body[0].userId).toEqual(user._id);
+    });
+
+    describe('Authentication check on GET /api/users/:userId/accounts', () => {
+      test('should return FORBIDDEN', async () => {
+        const res = await request(app).get(`/api/users/${user._id}/accounts`);
+        expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+      });
+    });
+  });
+
+  describe('GET /api/users/:userId/accounts/:accountId', () => {
+    test('should return all user accounts', async () => {
+      const res = await request(app).get(`/api/users/${user._id}/accounts/${account._id}`).set('Authorization', token);
+      expect(res.status).toEqual(200);
+      expect(res.body.userId).toEqual(user._id);
+      expect(res.body._id).toEqual(account._id);
+    });
+
+    describe('Authentication check on GET /api/users/:userId/accounts', () => {
+      test('should return FORBIDDEN', async () => {
+        const res = await request(app).get(`/api/users/${user._id}/accounts`);
+        expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+      });
+    });
+  });
+
+  describe('PATCH /api/users/:userId/accounts/:accountId', () => {
+    test('should return update user accounts', async () => {
+      const res = await request(app).patch(`/api/users/${user._id}/accounts/${account._id}`).set('Authorization', token).send({ isActive: false });
+      expect(res.status).toEqual(200);
+      expect(res.body.isActive).toEqual(false);
+      expect(res.body._id).toEqual(account._id);
+    });
+
+    test('Authentication check should return FORBIDDEN', async () => {
+      const res = await request(app).patch(`/api/users/${user._id}/accounts/${account._id}`);
+      expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+    });
+  });
+
+  // ================Credit cards=================
+
+  describe('POST /api/users/:userId/accounts/:accountId/credit-cards', () => {
+    test('should create a creditCard ', async () => {
+      const res = await request(app).post(`/api/users/${user._id}/accounts/${account._id}/credit-cards`).set('Authorization', token);
+      creditCard = res.body;
+      expect(res.status).toEqual(200);
+      expect(res.body.userId).toEqual(user._id);
+      expect(res.body.accountId).toEqual(account._id);
+    });
+
+    describe('Authentication check on POST /api/users/:userId/accounts', () => {
+      test('should return FORBIDDEN', async () => {
+        const res = await request(app).post(`/api/users/${user._id}/accounts/${account._id}/credit-cards`);
+        expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+      });
+    });
+  });
+
+  // describe('GET /api/users/:userId/accounts', () => {
+  //   test('should return all user accounts', async () => {
+  //     const res = await request(app).get(`/api/users/${user._id}/accounts`).set('Authorization', token);
   //     expect(res.status).toEqual(200);
-  //     expect(res.body[0].email).toEqual(user.email);
+  //     expect(res.body[0].userId).toEqual(user._id);
+  //   });
+
+  //   describe('Authentication check on GET /api/users/:userId/accounts', () => {
+  //     test('should return FORBIDDEN', async () => {
+  //       const res = await request(app).get(`/api/users/${user._id}/accounts`);
+  //       expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+  //     });
   //   });
   // });
 
-  // describe('GET /api/users/:id', () => {
-  //   it('should return a single user by id', async () => {
-  //     const res = await request(app).get(`/api/users/${user._id}`);
+  // describe('GET /api/users/:userId/accounts/:accountId', () => {
+  //   test('should return all user accounts', async () => {
+  //     const res = await request(app).get(`/api/users/${user._id}/accounts/${account._id}`).set('Authorization', token);
   //     expect(res.status).toEqual(200);
-  //     expect(res.body.name).toEqual(user.name);
+  //     expect(res.body.userId).toEqual(user._id);
+  //     expect(res.body._id).toEqual(account._id);
   //   });
 
-  //   it('should return 404 if user is not found', async () => {
-  //     const res = await request(app).get('/api/users/99');
-  //     expect(res.status).toEqual(404);
+  //   describe('Authentication check on GET /api/users/:userId/accounts', () => {
+  //     test('should return FORBIDDEN', async () => {
+  //       const res = await request(app).get(`/api/users/${user._id}/accounts`);
+  //       expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
+  //     });
   //   });
   // });
 
-  // describe('POST /api/users/login', () => {
-  //   it('should return a jwt', async () => {
-  //     const res = await request(app).post('/api/users/login').send({ email: user.email, password: user.password });
+  // describe('PATCH /api/users/:userId/accounts/:accountId', () => {
+  //   test('should return update user accounts', async () => {
+  //     const res = await request(app).patch(`/api/users/${user._id}/accounts/${account._id}`).set('Authorization', token).send({ isActive: false });
   //     expect(res.status).toEqual(200);
+  //     expect(res.body.isActive).toEqual(false);
+  //     expect(res.body._id).toEqual(account._id);
   //   });
 
-  //   it('Should return code 401 on wrong credentials', async () => {
-  //     const res = await request(app).post('/api/users/login').send({ email: user.email, password: 'notpassword' });
-  //     expect(res.status).toEqual(401);
-  //   });
-  // });
-
-  // describe('PATCH /api/users/:id', () => {
-  //   it('should update an existing user', async () => {
-  //     const updatedUser = {
-  //       email: 'slimane.berrada.01@gmail.com',
-  //       password: 'password',
-  //       name: 'Slimane',
-  //       agency: agency._id,
-  //     };
-  //     const res = await request(app)
-  //       .patch(`/api/users/${user._id}`)
-  //       .send(updatedUser);
-  //     expect(res.status).toEqual(200);
-  //     expect(res.body.name).toEqual(updatedUser.name);
-  //   });
-
-  //   it('should return 404 if user is not found', async () => {
-  //     const res = await request(app).put('/api/users/99').send({ name: 'Slimane' });
-  //     expect(res.status).toEqual(404);
-  //   });
-  // });
-
-  // describe('DELETE /api/users/:id', () => {
-  //   it('should delete an existing user', async () => {
-  //     const res = await request(app).delete(`/api/users/${user._id}`);
-  //     expect(res.status).toEqual(200);
-  //   });
-
-  //   it('should return 404 if user is not found', async () => {
-  //     const res = await request(app).delete('/api/users/99');
-  //     expect(res.status).toEqual(404);
+  //   test('Authentication check should return FORBIDDEN', async () => {
+  //     const res = await request(app).patch(`/api/users/${user._id}/accounts/${account._id}`);
+  //     expect(res.status).toEqual(httpStatus.UNAUTHORIZED);
   //   });
   // });
 });
