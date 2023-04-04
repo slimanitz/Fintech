16fec7f7390ae36220b369e8f273175fa0140954
slimanePLF <slimaneber2001@gmail.com>
2023-04-04 23:52:50 +0200

    Adding more timeout for the requests

--- server/config/server.js
+++ server/config/server.js
@@ -4,11 +4,13 @@ require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const expressMonitor = require('express-status-monitor');
+const timeout = require('connect-timeout');
 const router = require('../api/routes');
 const { errorHandler } = require('../middlewares/error');
 
 const app = express();
 app.use(cors({ origin: '*' }));
+app.use(timeout('10s'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
 app.use(expressMonitor());
