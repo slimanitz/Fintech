6c6d34aed10a5bb77ad77ea989f207f805bfb088
slimanePLF <slimaneber2001@gmail.com>
2023-04-05 06:55:48 +0200

    Updating simulation

--- server/scripts/multithread/worker.js
+++ server/scripts/multithread/worker.js
@@ -5,8 +5,6 @@ const axios = require('axios');
 const { host } = require('../../config/vars');
 const { transactionGatewayEnum, accountTypesEnum } = require('../../utils/enums');
 
-const counter = 0;
-
 const simulation = async ({ email, password }, accounts) => {
   try {
     const start = Date.now();
@@ -28,7 +26,7 @@ const simulation = async ({ email, password }, accounts) => {
 
     const account = accountResponse.data;
 
-    // STEP3 Make 3  random transactions
+    // STEP3 Make 4  random transactions with bank accounts
 
     for (let index = 0; index < 4; index += 1) {
       const creditAcccount = accounts[Math.floor(Math.random() * accounts.length)];
@@ -37,6 +35,28 @@ const simulation = async ({ email, password }, accounts) => {
         amount: 2000,
         gateway: transactionGatewayEnum.TRANSFER,
         creditAccountIban: creditAcccount._doc.iban,
+        comment: 'Test transaction ',
+      };
+
+      await instance.post(`/users/${user._id}/accounts/${account._id}/transactions`, payload);
+    }
+
+    const creditCard = await instance.post(`/users/${user._id}/accounts/${account._id}/credit-cards`);
+
+    // STEP4 make 4 random transactions with credit card
+
+    for (let index = 0; index < 4; index += 1) {
+      const creditAcccount = accounts[Math.floor(Math.random() * accounts.length)];
+
+      const payload = {
+        amount: 2000,
+        gateway: transactionGatewayEnum.CREDIT_CARD,
+        creditAccountIban: creditAcccount._doc.iban,
+        creditCardInfo: {
+          number: creditCard.data.number,
+          securityCode: creditCard.data.securityCode,
+          expirationDate: creditCard.data.expirationDate,
+        },
         comment: 'Test transaction ',
       };
 
