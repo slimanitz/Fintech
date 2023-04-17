bc9084daf932398a44ae6a82afdd821715e5b5b8
slimanePLF <slimaneber2001@gmail.com>
2023-04-17 00:32:49 +0000

    Sequelize fixes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const { redisClient } = require('./config/cache');
-const { connect } = require('./config/database');
+const { connect, sequelize } = require('./config/database');
 const app = require('./config/server');
 const { host } = require('./config/vars');
 const transactionCron = require('./crons/transaction-validation');
 
 app.listen(8080, async () => {
   await connect();
+  // await sequelize.sync({ force: true });
   console.log(redisClient.status);
   console.log(`The server is running on ${host}`);
   // transactionCron.start();
