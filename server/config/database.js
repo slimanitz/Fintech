62069b210542870907d7da55dc99ef457dec935d
slimanePLF <slimaneber2001@gmail.com>
2023-05-21 18:56:02 +0200

    test

--- server/config/database.js
+++ server/config/database.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(
 const connect = async () => {
   try {
     await sequelize.authenticate();
+
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
