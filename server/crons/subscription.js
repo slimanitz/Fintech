53e0bc1cbe0718d2e04309a1dc82e998066aa3fc
slimanePLF <slimaneber2001@gmail.com>
2023-05-21 18:52:28 +0200

    WIP: subscription cron

--- server/crons/subscription.js
+++ server/crons/subscription.js
@@ -1,15 +1,23 @@
 const cron = require('node-cron');
 const moment = require('moment');
 const Subscription = require('../api/models/subscription');
+const { subscriptionFrequency } = require('../utils/enums');
 
 const currentDate = moment();
 
-const SubscriptionCron = cron.schedule('*/4 * * * * *', async () => {
+const SubscriptionCron = cron.schedule('*/5 * * * * *', async () => {
   const subscriptions = await Subscription.findAll({
     where:
      { isCancelled: false },
     raw: true,
     nest: true,
+    limit:10
+  });
+
+  subscriptions.forEach(subscription => {
+    if(subscription.frequency == subscriptionFrequency.DAILY && lastTransaction)
+
+    
   });
 
   console.log('====================================');
