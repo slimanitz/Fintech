fb2c3df055baaeecaa321e0d65bdb312400e21db
slimanePLF <slimaneber2001@gmail.com>
2023-05-21 19:00:33 +0200

    Quick fix

--- server/config/database.js
+++ server/config/database.js
@@ -11,7 +11,6 @@ const sequelize = new Sequelize(
 const connect = async () => {
   try {
     await sequelize.authenticate();
-
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
