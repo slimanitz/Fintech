dcdea4ea4f3a06b8c11c339bbe5d560002a98aef
slimanePLF <slimaneber2001@gmail.com>
2023-05-24 17:37:04 +0100

    Enabling all the crons

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const { redisClient } = require('./config/cache');
 const { connect, sequelize } = require('./config/database');
 const app = require('./config/server');
 const { host } = require('./config/vars');
-const SubscriptionCron = require('./crons/subscription');
-// const transactionCron = require('./crons/transaction-validation');
+const SubscriptionCron = require('./crons/subscription-to-transaction');
+const transactionCron = require('./crons/transaction-validation');
 
 app.listen(8080, async () => {
   await connect();
