42db8ff99b674385bd7b03d2de9e6032afca7855
slimanePLF <slimaneber2001@gmail.com>
2023-06-05 17:54:47 +0100

    Initialising queue connection and subject Creation

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const { redisClient } = require('./config/cache');
 const { connect, sequelize } = require('./config/database');
+const rabbitMqClient = require('./config/rabbitmq');
 const { connectQueue } = require('./config/rabbitmq');
 const app = require('./config/server');
 const { host } = require('./config/vars');
@@ -8,7 +9,8 @@ const transactionCron = require('./crons/transaction-validation');
 
 app.listen(8080, async () => {
   await connect();
-  await connectQueue();
+  await rabbitMqClient.connectQueue();
+  await rabbitMqClient.createSubject('transactions');
 
   // await sequelize.sync({ force: true });
   console.log(redisClient.status);
